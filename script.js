61120ee68c58da429cab0d29b0ddbb1a9237cb99
Ali-noor Meraj <alinoormeraj.fm@gmail.com>
2022-02-28 02:27:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 setTimeout(function() {
 		const sheets = document.styleSheets;
 		const myframe = document.getElementById("twitter-widget-0");
-		console.log(myframe.contentWindow.document.head);
+		//console.log(myframe.contentWindow.document.head);
 		const link = document.createElement("link");
-		link.href = "https://a11.ventures/wp-content/themes/Divi/iframe.css";
+		link.href = "YOUR_CSS_FILE_LINK";
 		link.rel = "stylesheet";
 		myframe.contentWindow.document.head.appendChild(link);
 }, 7000);
